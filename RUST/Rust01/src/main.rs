677721cbdd286c4a445bca008f0b61ff3a90d9ec
Prydwen-kun <118287298+Prydwen-kun@users.noreply.github.com>
2025-04-10 11:43:57 +0200

    Update main.rs

--- RUST/Rust01/src/main.rs
+++ RUST/Rust01/src/main.rs
@@ -1,15 +1,20 @@
+fn main() {
+    let mut array: [i32; 6] = [1, 5, 6, 7, 4, 3];
 
-fn main(){
-    let mut s: String = String::new();
-    let mut i: i32 = 0;
-    while i < 10 {
-        s.push_str("Hello, world!\n");
-        i += 1;
+    loop {
+        let mut swapped: bool = false;
+
+        for i in 0..array.len() - 1 {
+            if array[i] > array[i + 1] {
+                array.swap(i, i + 1);
+                swapped = true;
+            }
+        }
+
+        if !swapped {
+            break;
+        }
     }
-    println!("{}", s);
-    let mut s: String = String::new();
-    let stdin: std::io::Stdin = std::io::stdin();
-    println!("Please enter a string:");
-    stdin.read_line(&mut s).expect("Failed to read line");
-    println!("You entered: {}", s);
+
+    println!("Sorted array: {:?}", array);
 }
